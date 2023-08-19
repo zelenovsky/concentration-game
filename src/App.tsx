d751e105d1e72c99620a80f44b6e7a503b11e85c
Artem Zelenov <deeloyy@gmail.com>
2023-08-19 16:22:25 +0600

    senior

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,5 @@
-import React from 'react'
-import { Card } from './components/Card'
-import { FlipsCounter } from './components/FlipCounter'
-import { GameOver } from './components/GameOver'
-
-import type { ICard } from './components/Card'
-
 export function App() {
-  const [cards] = React.useState<ICard[]>([])
-  const [flipsCount] = React.useState<number>(0)
-  const [isGameOver] = React.useState<boolean>(false)
-
-  const createCards = () => {
-    // write code here
-  }
-
-  const handleCardClick = () => {
-    // write code here
-  }
-
-  const handlePlayAgain = () => {
-    // write code here
-  }
-
-  React.useEffect(createCards, [])
-
   return (
-    <div className='flex flex-col items-center justify-center gap-5 min-h-screen py-6'>
-      <FlipsCounter count={flipsCount} />
-
-      {isGameOver && <GameOver onClick={handlePlayAgain} />}
-
-      <div className='grid grid-cols-3 lg:grid-cols-4 gap-6'>
-        {cards.map(card => (
-          <Card
-            key={card.id}
-            {...card}
-            onClick={handleCardClick}
-          />
-        ))}
-      </div>
-    </div>
+    <div />
   )
 }
--- src/constants.ts
+++
@@ -1,3 +0,0 @@
-export const NUMBER_OF_CARDS = 10
-export const NUMBER_OF_PAIRS = Math.floor(NUMBER_OF_CARDS / 2)
-export const TRANSITION_TIME = 500
