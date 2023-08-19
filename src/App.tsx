4b6685d44f5ad94318aa81075eba36f8b16d5e7c
Artem Zelenov <deeloyy@gmail.com>
2023-08-19 16:16:14 +0600

    middle

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
 import React from 'react'
-import classNames from 'classnames'
-import { generateId, randomChoice, shuffle, pause } from './utils'
-import emojis from './emojis'
-import { NUMBER_OF_PAIRS, TRANSITION_TIME } from './constants'
 import { Card } from './components/Card'
 import { FlipsCounter } from './components/FlipCounter'
 import { GameOver } from './components/GameOver'
@@ -10,103 +6,20 @@ import { GameOver } from './components/GameOver'
 import type { ICard } from './components/Card'
 
 export function App() {
-  const [cards, setCards] = React.useState<ICard[]>([])
-  const [facedUpCard, setFacedUpCard] = React.useState<ICard | null>(null)
-  const [isLocked, setLock] = React.useState<boolean>(false)
-  const [flipsCount, setFlipsCount] = React.useState<number>(0)
-  const [isGameOver, setIsGameOver] = React.useState<boolean>(false)
-  const totalMatchedCount = React.useRef<number>(0)
-
-  const chooseCard = async (currentCard: ICard) => {
-    // click to current open card
-    if (facedUpCard && facedUpCard.id === currentCard.id) return
-
-    setFlipsCount(flipsCount + 1)
-
-    // first time open card
-    if (!facedUpCard) {
-      setFacedUpCard(currentCard)
-      setCards(cards.map(card => {
-        if (card.id === currentCard.id) return { ...card, isFaceUp: true }
-        return card
-      }))
-    }
-
-    // second time open card - matched
-    if (facedUpCard && facedUpCard.emoji === currentCard.emoji) {
-      setLock(true)
-      setFacedUpCard(null)
-      totalMatchedCount.current += 1
-
-      setCards(cards.map(card => {
-        if (card.id === currentCard.id) return { ...card, isFaceUp: true }
-        return card
-      }))
-
-      await pause(TRANSITION_TIME)
-
-      setCards(cards.map(card => {
-        if (card.id === facedUpCard.id) return { ...card, isMatched: true }
-        if (card.id === currentCard.id) return { ...card, isMatched: true, isFaceUp: true }
-        return { ...card, isFaceUp: false }
-      }))
-
-      if (totalMatchedCount.current === NUMBER_OF_PAIRS) {
-        setIsGameOver(true)
-      }
-
-      setLock(false)
-    }
-
-    // second time open card - unmatched
-    if (facedUpCard && facedUpCard.emoji !== currentCard.emoji) {
-      setLock(true)
-      setFacedUpCard(null)
-
-      setCards(cards.map(card => {
-        if (card.id === currentCard.id) return { ...card, isFaceUp: true }
-        return card
-      }))
-
-      await pause(TRANSITION_TIME)
-
-      setCards(cards.map(card => {
-        if (card.id === facedUpCard.id) return { ...card, isFailure: true }
-        if (card.id === currentCard.id) return { ...card, isFaceUp: true, isFailure: true }
-        return card
-      }))
-
-      await pause(TRANSITION_TIME)
-
-      setCards(cards.map(card => ({ ...card, isFaceUp: false, isFailure: false })))
-      setLock(false)
-    }
-  }
+  const [cards] = React.useState<ICard[]>([])
+  const [flipsCount] = React.useState<number>(0)
+  const [isGameOver] = React.useState<boolean>(false)
 
   const createCards = () => {
-    let tempCards: ICard[] = []
-
-    Array.from(Array(NUMBER_OF_PAIRS)).forEach(() => {
-      const newCard: Omit<ICard, 'id'> = {
-        emoji: randomChoice(emojis),
-        isFaceUp: false,
-        isMatched: false,
-        isFailure: false
-      }
-      tempCards = [...tempCards, { ...newCard, id: generateId() }, { ...newCard, id: generateId() }]
-    })
-
-    setCards(shuffle(tempCards))
+    // write code here
   }
 
-  const handleCardClick = (card: ICard) => () => {
-    chooseCard(card)
+  const handleCardClick = () => {
+    // write code here
   }
 
   const handlePlayAgain = () => {
-    setIsGameOver(false)
-    setFlipsCount(0)
-    createCards()
+    // write code here
   }
 
   React.useEffect(createCards, [])
@@ -117,14 +30,12 @@ export function App() {
 
       {isGameOver && <GameOver onClick={handlePlayAgain} />}
 
-      <div className={classNames('grid grid-cols-3 lg:grid-cols-4 gap-6', {
-        'pointer-events-none': isLocked
-      })}>
+      <div className='grid grid-cols-3 lg:grid-cols-4 gap-6'>
         {cards.map(card => (
           <Card
             key={card.id}
             {...card}
-            onClick={handleCardClick(card)}
+            onClick={handleCardClick}
           />
         ))}
       </div>
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,42 +1,22 @@
 import React from 'react'
-import classNames from 'classnames'
 
 export interface ICard {
   id: number
   emoji: string
-  isFaceUp: boolean
-  isMatched: boolean
-  isFailure: boolean
 }
 
 export interface IProps extends ICard {
   onClick(): void
 }
 
-export const Card = React.memo(({ emoji, isMatched, isFaceUp, isFailure, onClick }: IProps) => {
+export const Card = React.memo(({ emoji, onClick }: IProps) => {
   return (
     <button
       type='button'
-      className={classNames(`relative block w-52 h-60 transition-opacity duration-300 perspective`, {
-        'pointer-events-none': isMatched,
-        'opacity-60': isMatched,
-        'animate-shake': isFailure
-      })}
+      className='relative block w-52 h-60 transition-opacity duration-300 perspective'
       onClick={onClick}
     >
-      <div
-        className={classNames(`absolute inset-0 z-10 flex items-center justify-center text-6xl bg-gray-300 border-2 border-gray-400 rounded-lg transition-transform duration-300 -rotate-y-180 backface-hidden`, {
-          '-rotate-y-360': isMatched || isFaceUp
-        })}
-      >
-        {emoji}
-      </div>
-
-      <div
-        className={classNames(`absolute inset-0 z-0 bg-gray-300 border-2 border-gray-400 rounded-lg transition-transform duration-300`, {
-          '-rotate-y-180': isMatched || isFaceUp
-        })}
-      />
+      {emoji}
     </button>
   )
 })
